59927ae58d1d08dbc25c654679631a5972dacacf
DenisaO <ondrovadenisa95@gmail.com>
2023-03-08 20:24:47 +0100

    Hotový domácí úkol 1,2

--- main.js
+++ main.js
@@ -1,48 +1,74 @@
-// Obor platnosti proměnných
-
-const age = Number(prompt('Zadej svůj věk:')) // Globální obor platnosti 
-
+//ukol 1
 /*
-if (age < 18) {
-	const remains = 18 - age
 
-	if (remains >= 2) {
-		document.body.innerHTML = '<p>Už to máš za pár</p>'
-	} else if (remains >= 5) {
-		document.body.innerHTML = `<p>${age}</p>` // V pořádku
-		document.body.innerHTML += `<p>Ještě si počkáš ${remains} let</p>`
-	} else {
-		document.body.innerHTML = '<p>Utíkej za mamkou</p>'
-	}
-} else {
-	document.body.innerHTML = `<p>${age}</p>` // V pořádku
-	document.body.innerHTML += '<p>Vítej mezi dospěláky</p>'
+const salary = (wage, hours, days) => {
+	return Math.round(wage * hours * days)
 }
 
-document.body.innerHTML = `<p>${age}</p>` // V pořádku
+const tax = (salary, taxPercentage) => {
+	return Math.round((salary * (100 - taxPercentage)) / 100)
+}
+const taxPercentage = 15
+const wage= prompt('Enter your wage/hour value:');
+const hours= prompt('Enter working hours per day:');
+const days= prompt('Enter working days in a month:');
+const noTax= salary(wage, hours, days)
+const afterTax= tax(noTax, taxPercentage)
+
+salary();
+tax();
+
+document.body.innerHTML =
+`<h1>Salary after tax count:</h1>
+<h3>Your entered values:</h3>
+<p>Wage per hour: ${wage} Kč</p>
+<p>Hours: ${hours}</p>
+<p>Days: ${days}</p>
+<br>
+<h3>Calculation:</h3>
+<p>No Tax: ${noTax} Kč</p>
+<p>After Tax: ${afterTax} Kč</p>`
 */
 
-const superMessage = 'moje super zpráva'
+//ukol 2
 
-if (age < 18) {
-	const message = 'Utíkej za mamkou' // lokální obor platnosti
-	document.body.innerHTML = `<p>${message}</p>`
-    
-    console.log(message)
-} else {
-	const message = 'Vítej mezi dospěláky' // lokální obor platnosti
-	document.body.innerHTML = `<p>${message}</p>`
-    console.log(message)
+const plusButton = document.querySelector('.plus');
+const minusButton = document.querySelector('.minus');
+const multiplyButton = document.querySelector('.multiply');
+const divideButton = document.querySelector('.divide');
+const finalResult = document.querySelector('#number3');
+const calculateButton = document.querySelector('.result');
+
+function buttonAction(button) {
+  if (button.classList.contains('active')) {
+    button.classList.remove('active');
+  } else {
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach(btn => btn.classList.remove('active'));
+    button.classList.add('active');
+  }
 }
 
-const message = 'Vítej ve světě slasti'
+const calculate = () => {
+  const number1 = document.querySelector('#number1').value;
+  const number2 = document.querySelector('#number2').value;
+  const num1 = parseFloat(number1);
+  const num2 = parseFloat(number2);
+  const operation = document.querySelector('.active').textContent;
+  let result;
 
-const checkAge = (age, message) => {
-    if(age < 18) {
-        return message
-    }else{
-        const message = 'Vítej mezi dospěláky'
-        return message
-    }
-}
+  if (operation === '+') {
+    result = num1 + num2;
+  } else if (operation === '-') {
+    result = num1 - num2;
+  } else if (operation === '*') {
+    result = num1 * num2;
+  } else if (operation === ':') {
+    result = num1 / num2;
+  }
+
+  finalResult.value = result;
+};
+
+calculateButton.addEventListener('click', calculate);
 
